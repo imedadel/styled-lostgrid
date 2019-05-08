6774e7a9312446ab1f7aa8a0601c4cfc7a200128
Imed Adel <aadelimed@gmail.com>
2019-05-09 00:06:22 +0100

    Import column function

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {render} from 'react-dom'
+import styled from "styled-components"
 
-import Example from '../../src'
+import grd from '../../src'
 
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>styled-lostgrid Demo</h1>
-      <Example/>
-    </div>
-  }
-}
+const Div1 = styled.div`
+  ${grd(`2/3`)};
+`
+const Div2 = styled.div`
+  ${grd(`1/3`)};
+`
+
+const Demo = () => (
+  <div>
+    <Div1>
+      One
+    </Div1>
+    <Div2>
+      Two
+    </Div2>
+  </div>
+)
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React, {Component} from 'react'
+import React from "react";
+import { css } from "styled-components";
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+export default x => css`
+  width: calc(99.9% * ${x} - (30px - 30px * ${x}));
+  
+  &:nth-child(1n) {
+    float: left;
+    margin-right: 30px;
+    clear: none;
   }
-}
+  &:last-child {
+    margin-right: 0;
+  }
+  &:nth-child(${x[2]}n) {
+    margin-right: 0;
+    float: right;
+  }
+  &:nth-child(${x[2]}n + 1) {
+    clear: both;
+  }
+`;
