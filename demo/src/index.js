fc33401d1ee6c5adcd092c968659795e3d29804f
Imed Adel <aadelimed@gmail.com>
2019-05-10 02:08:53 +0100

    Set up basic testing

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {render} from 'react-dom'
 import styled from "styled-components"
 
-import grd from '../../src'
+import lost from '../../src'
 
 const Div1 = styled.div`
-  ${grd(`2/3`,`2`,`20px`,`flex`)};
+  ${lost.column(`2/3`)};
 `
 const Div2 = styled.div`
-  ${grd(`1/3`)};
+  ${lost.column(`1/3`)};
 `
 
 const Demo = () => (
